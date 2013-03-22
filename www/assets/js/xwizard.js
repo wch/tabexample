534bdbeec5ae7804ce6b5eeb50bea0b621a8ea09
Winston Chang <winston@stdout.org>
2013-03-22 12:07:22 -0500

    Initial version

---
+++ www/assets/js/xwizard.js
@@ -0,0 +1,27 @@
+//
+// Shiny ammends
+//
+
+$(".wizardnav-next").click(function(){
+    var currdiv = $(this).parent().closest(".tab-pane").attr("id");
+    var currid = parseInt(currdiv.split("step")[1]);
+    disablePanel(currid);
+    navigateToPanel(currid + 1);
+});
+
+$(".wizardnav-prev").click(function(){
+    var currdiv = $(this).parent().closest(".tab-pane").attr("id");
+    var currid = parseInt(currdiv.split("step")[1]);
+    disablePanel(currid);
+    navigateToPanel(currid - 1);
+});
+
+function disablePanel(panelid) {
+    $("#step" + panelid).removeClass("active");
+    $("#lbl-step" + panelid).removeClass("active");
+}
+
+function navigateToPanel(panelid) {
+    $("#step" + panelid).addClass("active");
+    $("#lbl-step" + panelid).addClass("active");
+}
